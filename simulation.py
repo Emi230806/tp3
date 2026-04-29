05f99eebc9c5d962b1555218571332dfa95b5a7e
Emiliano <e6321450@cmaisonneuve.qc.ca>
2026-04-29 09:38:32 -0400

    first commit

---
+++ simulation.py
@@ -0,0 +1,18 @@
+import numpy as np 
+import tkinter as tk 
+
+largeur = 1000
+hauteur = 800
+r = 14
+delta_t = 0.5
+
+class EspaceDeJeu :
+    def __init__(self, root) :
+        self.root = root
+        self.root.title("Simulation de Billard")
+        
+
+        
+
+
+
